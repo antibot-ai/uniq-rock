388faccc907142304af80600a26c50cf915757c2
uriid1 <appdurov@gmail.com>
2024-06-25 20:42:58 +0300

    Add readme and tests

--- lua/uniq/init.lua
+++ lua/uniq/init.lua
@@ -1,16 +1,25 @@
 ---
--- Удаление дубликатов из таблицы
+-- Удаление дубликатов из массива
+-- @module uniq
 local uniq
 
---- Удаляет дубликаты из таблицы
--- @param arr Исходная таблица
+--- Удаляет дубликаты из массива
+-- @param arr (table) Исходная таблица
 -- @return Новая таблица с уникальными элементами
+-- @usage
+  -- local arr = {nil, 1, 1, 2, 2, nil, nil, 3, 3}
+  -- local new_arr = uniq(arr)
+  --
+  -- -- 1, 2, 3
+  -- for i = 1, #new_arr do
+  --   print(new_arr[i])
+  -- end
 function uniq(arr)
   local ret = {}
   local hash = {}
 
-  for _, val in pairs(arr) do
-    if (hash[val] == nil) then
+  for _, val in next, arr do
+    if (val) and (hash[val] == nil) then
       hash[val] = true
       table.insert(ret, val)
     end
